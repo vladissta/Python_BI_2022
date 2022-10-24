6d6c95a74374d0256bce21110305985edded70b1
vladissta <staninvladislav@mail.ru>
2022-10-24 18:13:20 +0300

    final without docstrings

--- numpy_hw/numpy_challenge.py
+++ numpy_hw/numpy_challenge.py
@@ -17,29 +17,26 @@ def compute_2d_distance(a, b):
     return np.sqrt(((a - b) ** 2).sum())
 
 
-def compute_multidimensional_distance():
+def compute_multidimensional_distance(a, b):
     return np.linalg.norm(a - b)
 
 
-def compute_pair_distances():
-    pass
+def compute_pair_distances(a):
+    dist_mat = np.zeros((a.shape[0], a.shape[0]))
+
+    row, column = np.triu_indices(3, k=1)
+
+    differences = a[row] - a[column]
+    sums_of_squares = (differences ** 2).sum(axis=1)
+    distances = np.sqrt(sums_of_squares)
+
+    dist_mat[row, column] = distances
+    dist_mat += dist_mat.transpose()
+
+    return dist_mat
 
 
 if __name__ == '__main__':
     arr1 = np.array([1, 2, 3, 4, 5, 6])
     arr2 = np.arange(1, 7)
-    arr3 = (np.ones(6) + np.arange(0, 6)).astype('int')  # убери
-
-    a = arr1.reshape(2, 3)
-    b = arr1.reshape(3, 2)
-    c = np.arange(1, 3)
-    v1 = np.array([1, 1])
-    v2 = np.array([0, 0])
-
-    print(
-        multiplication_check(a, b),
-        matrix_multiplication(a, b),
-        multiply_matrices(a, b, c),
-        compute_2d_distance(v1, v2),
-        sep='\n'
-    )
+    arr3 = np.ones(6)
