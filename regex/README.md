041903a5022c6fafdd21d8775f12b21d57af53c2
vladissta <staninvladislav@mail.ru>
2022-11-17 12:26:43 +0300

    regex hw

--- regex/README.md
+++ regex/README.md
@@ -1,16 +1,16 @@
-# REGULAR EXPRESSION HOMEWORK
+# Regular expressions homework
 
 ### About files:
 
-- regex_hw.py - script for review  
-- regex_hw.ipynb - notebook  
+- **regex_hw.py** - script for review  
+- **regex_hw.ipynb** - notebook  
 
-- ftp.txt - file with ftp links (output from task 1)  
-- nums.txt - output from task with numbers  (output from task 2)  
-- a_words.txt - output with words contain letter 'a' (output from task 3)  
-- exclamatory_sentences.txt - output with exclamatory sentences (output from task 4)  
+- **ftps.txt** - file with ftp links (output from task 1)  
+- **nums.txt** - output from task with numbers  (output from task 2)  
+- **a_words.txt** - output with words contain letter 'a' (output from task 3)  
+- **exclamatory_sentences.txt** - output with exclamatory sentences (output from task 4)  
 
-- requirements.txt - files with requirement pacakges  
+- **requirements.txt** - files with requirement pacakges  
 
 ```
 pip install -r requirements.txt
--- regex/regex_hw.py
+++ regex/regex_hw.py
@@ -10,20 +10,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-# ## Распарсите файл references при помощи регулярных выражений и запишите оттуда все ftp ссылки в файл ftps (5 баллов)
+# ## Распарсите файл references при помощи регулярных выражений и запишите оттуда все ftp ссылки в файл ftps
 
 # In[28]:
 
 
 with open('references.txt') as refs:
-    ftp = open('ftp.txt', 'w')
+    ftp = open('ftps.txt', 'w')
     for line in refs:
         for link in re.findall(r'ftp\..+?[;|\t|\n]\b', line.strip()):
             print(link.strip(';\t'), file=ftp)
 
-
-# ## Извлеките из рассказа  2430 A.D. все числа 
+# ## Извлеките из рассказа  2430 A.D. все числа
 
 # In[29]:
 
@@ -31,13 +29,12 @@ with open('references.txt') as refs:
 with open('2430AD.txt') as story:
     nums = open('nums.txt', 'w')
     for line in story:
-        match_num = re.findall(r'(?:\d+)',line)
+        match_num = re.findall(r'(?:\d+)', line)
         if match_num:
             for num in match_num:
                 print(num, file=nums)
 
-
-# ## Из того же рассказа извлеките все слова, в которых есть буква a, регистр при этом не важен  (5 баллов)
+# ## Из того же рассказа извлеките все слова, в которых есть буква a, регистр при этом не важен
 
 # In[30]:
 
@@ -45,13 +42,12 @@ with open('2430AD.txt') as story:
 with open('2430AD.txt') as story:
     a_words = open('a_words.txt', 'w')
     for line in story:
-        match = re.findall(r'\b[\w\']*[aA][\w\']*?\b',line)
+        match = re.findall(r'\b[\w\']*[aA][\w\']*?\b', line)
         if match:
             for word in match:
                 print(word, file=a_words)
 
-
-# ## Извлеките из рассказа все восклицательные предложения  (5 баллов)
+# ## Извлеките из рассказа все восклицательные предложения
 
 # In[31]:
 
@@ -59,13 +55,12 @@ with open('2430AD.txt') as story:
 with open('2430AD.txt') as story:
     exclamatory_sentences = open('exclamatory_sentence.txt', 'w')
     for line in story:
-        match = re.findall(r'([A-Z](?:[^[\.])*?!)',line)
+        match = re.findall(r'([A-Z](?:[^[\.])*?!)', line)
         if match:
             for word in match:
-                print(word, file = exclamatory_sentences)
+                print(word, file=exclamatory_sentences)
 
-
-# ## Постройте гистограмму распределения длин уникальных слов (без учёта регистра, длина от 1) в тексте. 
+# ## Постройте гистограмму распределения длин уникальных слов (без учёта регистра, длина от 1) в тексте.
 
 # ### Max length of word and list of counts
 
@@ -83,21 +78,19 @@ with open('2430AD.txt') as story:
             max_letters -= 1
             break
     print(max_letters)
-    
+
     lst_n_words = []
-    for nletters in range(1, max_letters+1):
+    for nletters in range(1, max_letters + 1):
         n_words = len(set(re.findall(fr'[^\w\'](([A-z\'](?!\')){{{nletters}}})\b', words.lower())))
         lst_n_words.append(n_words)
 
 print(lst_n_words)
 
-
 # In[33]:
 
 
-n_words_df = pd.DataFrame({"n_letters":range(1,16), "count":lst_n_words})
+n_words_df = pd.DataFrame({"n_letters": range(1, 16), "count": lst_n_words})
 n_words_df
-
 
 # In[34]:
 
@@ -130,12 +123,12 @@ brick_lang('всё равно татарский язык лучше всех')
 
 def counter_words_in_sentence(n, text):
     sentences = []
-    match = re.findall(fr'((([A-ZА-Я]([^ \.\?!]* ){{{n-1}}})([^ \.\?!]*))([\.\?!]))', text)
-    if match: 
+    match = re.findall(fr'((([A-ZА-Я]([^ \.\?!]* ){{{n - 1}}})([^ \.\?!]*))([\.\?!]))', text)
+    if match:
         for sentence in match:
             sentences.append(tuple(sentence[1].split()))
     return sentences
-    
+
 
 def n_word_sentence_finder(text, n, isfile=False):
     if isfile:
@@ -151,9 +144,7 @@ def n_word_sentence_finder(text, n, isfile=False):
 
 s = 'Здесь три слова. Тут тоже три!'
 
-
 # In[25]:
 
 
 n_word_sentence_finder(s, n=3, isfile=False)
-
