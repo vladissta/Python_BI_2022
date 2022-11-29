c83f698c728897f44f8c12c4c40f9376ba6532d8
vladissta <staninvladislav@mail.ru>
2022-11-29 23:13:02 +0300

    Documentation and tests

--- functional_hw/functional.py
+++ functional_hw/functional.py
@@ -4,14 +4,14 @@ import sys
 def sequential_map(*args):
     """
     sequential_map takes several functions, and container of values to which these functions will be applied
-    and return list of results after application all functions to values container
+    and return list of results after application all functions to values container.
 
-    [!] Functions should return application result of them
-    [!] Container of values should always be written last
-    [!] Functions apply in order that they were written
+    [!] Functions should return application results of them.
+    [!] Container of values should always be written last.
+    [!] Functions apply in order that they were written.
 
-    :param args: functions and container of values (last!)
-    :return: list with modified values from container
+    :param args: functions and container of values (last!).
+    :return: list with modified values from container.
     """
 
     *functions, values = args
@@ -25,14 +25,14 @@ def sequential_map(*args):
 def consensus_filter(*args):
     """
     consensus_filter takes several filtering functions, and container of values to which these functions will be applied
-    and return list of values that passed application of functions to them with returning True
+    and return list of values that passed application of functions to them with returning True.
 
-    [!] Functions should return True/False
-    [!] Container of values should always be written last
-    [!] Functions apply in order that they were written
+    [!] Functions should return True/False.
+    [!] Container of values should always be written last.
+    [!] Functions apply in order that they were written.
 
-    :param args: functions and container of values (last!)
-    :return: list with values from container which passed the filtration
+    :param args: functions and container of values (last!).
+    :return: list with values from container which passed the filtration.
     """
 
     *functions, values = args
@@ -45,15 +45,13 @@ def consensus_filter(*args):
 
 def conditional_reduce(conditional_fun, apply_fun, values):
     """
+    conditional_reduce takes values that filters with special function (conditional_fun)
+    and then applies another function (apply_fun) to two values passed the filter.
 
-    onditional_reduce takes values that filtering with special function (conditional_fun)
-    and then apply to two values passed the filter another function (apply_fun)
-
-
-    :param conditional_fun:
-    :param apply_fun:
-    :param values:
-    :return:
+    :param conditional_fun: filtering function.
+    :param apply_fun: function that apply to two values passed the filtering.
+    :param values: container of values.
+    :return: result of application [apply_fun] to filtered by [conditional_fun] values from container [values].
     """
 
     filtered_values = list(filter(conditional_fun, values))
@@ -61,40 +59,95 @@ def conditional_reduce(conditional_fun, apply_fun, values):
     return apply_fun(filtered_values[0], filtered_values[1])
 
 
-def func_chain(*functions):
+def func_chain(*args):
+    """
+    func_chain takes functions and return one new function that includes all taken functions.
+    This new function applies all taken functions in the given order to given values.
+
+    [!] Functions apply in order that they were written.
+    [!] Functions should return application results of them.
+
+    New function takes some values (*values)
+    and returns list of modified values or one value (not in list) depended on number of values in result.
+
+    :param args: functions.
+    :return: new function includes taken functions.
+    """
+
     def new_funct(*values):
-        for fun in functions:
+        for fun in args:
             values = list(map(fun, values))
+        if len(values) == 1:
+            return values[0]
         return values
 
     return new_funct
 
 
-def sequential_map_integrated(*args):
+def sequential_map_2(*args):
+    """
+    [This function have te functionality as sequential_map, but utilizes func_chain function in it.]
+
+    sequential_map_2 takes several functions, and container of values to which these functions will be applied
+    and return list of results after application all functions to values container.
+
+    [!] Functions should return application results of them.
+    [!] Container of values should always be written last.
+    [!] Functions apply in order that they were written.
+
+    :param args: functions and container of values (last!).
+    :return: list with modified values from container.
+    """
+
     *functions, values = args
     map_fun = func_chain(*functions)
 
     return map_fun(*values)
 
 
-def multiple_partial(*functions, **arguments):
-    list_of_functions = []
+def multiple_partial(*functions, **params):
+    """
+    multiple_partial takes functions (*functions) and modifies them with setting their parameters (*params) by default.
 
-    def funct_factory(funct_to_change):
+    multiple_partial includes func_factory function to create new independent functions with set parameters.
+
+    [!] If some modified function is used with new parameters which differ from set with multiple_partial,
+    modified function will use new parameters.
+
+    :param functions: functions to modify.
+    :param params: parameters to set default.
+    :return: list of modified functions.
+    """
+
+    def func_factory(funct_to_change):
         def partial_fun(*args, **kwargs):
-            arguments.update(kwargs)
-            return funct_to_change(*args, **arguments)
+            params.update(kwargs)
+            return funct_to_change(*args, **params)
 
         return partial_fun
 
+    list_of_functions = []
+
     for fun in functions:
-        new_fun = funct_factory(fun)
+        new_fun = func_factory(fun)
         list_of_functions.append(new_fun)
 
     return list_of_functions
 
 
 def super_print(*args, sep=' ', end='\n', file=None):
+    """
+    [The FULL COPY of base print function]
+
+    Prints the values to a stream, or to sys.stdout by default.
+
+    :param args: values to print.
+    :param sep: string inserted between values, default a space.
+    :param end: string appended after the last value, default a newline.
+    :param file: a file to write to.
+    :return: None
+    """
+
     text = (list(map(str, args)))
     stdout = sep.join(text) + end
 
@@ -104,21 +157,34 @@ def super_print(*args, sep=' ', end='\n', file=None):
 
 
 if __name__ == '__main__':
-    print(
-        sequential_map(lambda x: x ** 3, lambda x: x ** 0.5, [1, 2, 3, 4, 5]),
-        consensus_filter(lambda x: x > 3, lambda x: x < 6, [1, 2, 3, 4, 5]),
-        conditional_reduce(lambda x: x > 3, lambda x, y: x * y, [1, 2, 3, 4, 5]),
-        sequential_map_integrated(lambda x: x ** 3, lambda x: x ** 0.5, [1, 2, 3, 4, 5]),
-        sep='\n'
-    )
+    # SOME TESTING
 
-    my_fun = func_chain(lambda x: x + 2, lambda x: (x / 4, x // 4))
-    print(my_fun(37, 4))
+    assert sequential_map(lambda x: x ** 2, lambda x: x + 1, [1, 2, 3, 4, 5]) == [2, 5, 10, 17, 26]
+    assert sequential_map(lambda x: x + " wow", lambda x: x[-3:], ['abc', 'efg']) == ['wow', 'wow']
+
+    assert consensus_filter(lambda x: x > 3, lambda x: x < 6, [1, 2, 3, 4, 5]) == [4, 5]
+    assert consensus_filter(lambda x: len(x) > 3, lambda x: x[-1] == '!', ['wow!', 'how?', 'ok!']) == ['wow!']
+
+    assert conditional_reduce(lambda x: x > 3, lambda x, y: x * y, [1, 2, 3, 4, 5]) == 20
+    assert conditional_reduce(lambda x: len(x) > 3 and x[-1] == '!',
+                              lambda x, y: x + ' ' + y,
+                              ['wow!', 'how?', 'ok!', 'thanks!']) == "wow! thanks!"
+
+    assert sequential_map_2(lambda x: x ** 2, lambda x: x + 1, [1, 2, 3, 4, 5]) == [2, 5, 10, 17, 26]
+    assert sequential_map(lambda x: x + " wow", lambda x: x.upper(), ['one', 'two']) == ['ONE WOW', 'TWO WOW']
+
+    my_fun = func_chain(lambda x: x + 3, lambda x: (x / 4))
+    assert my_fun(37) == 10
+    assert my_fun(7, 17) == [2.5, 5]
 
     new_min, new_max = multiple_partial(min, max, key=lambda x: -x)
-    print(new_max(1, 2, 3), new_min(1, 2, 3))
+    assert new_max(1, 2, 3), new_min(1, 2, 3) == (1, 3)
+    assert new_max(1, 2, 3, key=lambda x: x), new_min(1, 2, 3, key=lambda x: x) == (3, 1)
 
-    super_print('What a pain, Argentina vs Jamaica:', 5, ":", 0, sep='  ', end='!')
+    # What a pain, Argentina vs Jamaica:	5	:	0 !
+    super_print('What a pain, Argentina vs Jamaica:', 5, ":", 0, sep='\t', end=' !')
+
+    # You can see result file in repository
     super_print('He said, "One day, you\'ll leave this world behind', 'So live a life you will remember"',
                 'My father told me when I was just a child', '"These are the nights that never die"',
                 sep='\n', file='Avicii.txt')
