92970c8f666d25f2960d61b77cdb0b1324da4e09
vladissta <staninvladislav@mail.ru>
2022-12-05 02:21:13 +0300

    1st version of wc and ls

---
+++ sys_os/ls.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('path', default='.', help='directory to show', type=str, nargs='*')
+parser.add_argument('-a', help='show all files', action='store_true')
+
+args = parser.parse_args()
+
+several_output = len(args.path) > 1
+
+for path in args.path:
+    if args.a:
+        sys.stdout.write(f'{path}:\n' * several_output)
+        sys.stdout.write('\t'.join([os.curdir, os.pardir] +
+                                   sorted(os.listdir(path))) +
+                         "\n\n" * several_output)
+    else:
+        sys.stdout.write(f'{path}:\n' * several_output)
+        sys.stdout.write(
+            '\t'.join(sorted([file for file in os.listdir(path)
+                              if not file.startswith('.')])) +
+            "\n\n" * several_output * (path != args.path[-1]))
